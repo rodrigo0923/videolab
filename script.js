9c757b3297d5cc4d2282787b3f4fe64f7b701d2b
rodrigo0923 <rodrigo.yanez1401@gmail.com>
2023-09-19 18:45:04 -0300

    videolab

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("page loaded...");
+var myVideo = document.getElementById("video1") 
+  function over(element) {
+    element.play;
+    console.log("play video corriendo");    
+}
+    
+function out(element) {
+    element.pause;
+    console.log("stop");    
+}
+
